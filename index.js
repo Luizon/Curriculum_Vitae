504f3db7e4a38ef4701961498a7a333fc516c656
P Luizon <pluizoncv@gmail.com>
2020-05-13 20:04:04 -0600

    fixing things on responsive design
    debería dejar de hacer comentarios en inglés si el html está en español, luego reparo eso

---
+++ index.js
@@ -0,0 +1,6 @@
+// Quizá no es necesario un archivo extra sólo para activar los popover, pero la costumbre me dice que no meta programación en el HTML
+$(document).ready(
+	function() {
+		$('[data-toggle="popover"]').popover();
+	}
+);
